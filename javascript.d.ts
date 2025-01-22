e8820a46c6647e925723eb83c273a5fca2b97395
Chance Strickland <hi@chance.dev>
2025-01-22 08:55:24 -0800

    js: deprecate `config` and `languageOptions`
    Use `getConfig` and `getLanguageOptions` instead. Allows for setting specific
    environment globals.

--- javascript.d.ts
+++ javascript.d.ts
@@ -1,5 +1,13 @@
 import type { Linter } from "eslint";
 export const rules: Partial<Linter.RulesRecord>;
 export const files: string[];
+/** @deprecated */
 export const languageOptions: Linter.LanguageOptions;
+/** @deprecated */
 export const config: Linter.Config;
+
+export function getLanguageOptions(
+	globals?: Linter.Globals,
+): Linter.LanguageOptions;
+
+export function getConfig(globals?: Linter.Globals): Linter.Config;
--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// @ts-check
 import globals from "globals";
 import { ecmaVersion } from "./lib/constants.js";
 import rules from "./lib/rules-core.js";
@@ -16,7 +17,10 @@ export const files = [
 	"**/*.cts",
 ];
 
-/** @type {import('eslint').Linter.LanguageOptions} */
+/**
+ * @type {import('eslint').Linter.LanguageOptions}
+ * @deprecated
+ */
 export const languageOptions = {
 	sourceType: "module",
 	ecmaVersion,
@@ -28,9 +32,41 @@ export const languageOptions = {
 	},
 };
 
-/** @type {import('eslint').Linter.Config} */
+/**
+ * @type {import('eslint').Linter.Config}
+ * @deprecated
+ */
 export const config = {
 	files,
 	languageOptions,
 	rules,
 };
+
+/**
+ * @param {import('eslint').Linter.Globals} [globals]
+ * @returns {import('eslint').Linter.LanguageOptions}
+ */
+export function getLanguageOptions(globals) {
+	return {
+		sourceType: "module",
+		ecmaVersion,
+		globals,
+		parserOptions: {
+			ecmaFeatures: {
+				jsx: true,
+			},
+		},
+	};
+}
+
+/**
+ * @param {import('eslint').Linter.Globals} [globals]
+ * @returns {import('eslint').Linter.Config}
+ */
+export function getConfig(globals) {
+	return {
+		files,
+		languageOptions: getLanguageOptions(globals),
+		rules,
+	};
+}
